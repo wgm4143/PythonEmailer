2ce860f05ea794697b3a707e083074bf03557c24
Wilson <wgm4143@gmail.com>
2019-10-29 15:55:58 -0400

    Initial commit

---
+++ emailer.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+SENDER_EMAIL = "from@email.com"
+SENDER_PASS = "password"
+SENDER_SMTP = "smtp.email.com"
+
+def emailer(target, subject, contents):
+    msg = MIMEMultipart('alternative')
+    msg['From'] = SENDER_EMAIL
+    msg['To'] = target
+    msg['Subject'] = subject
+    #text = MIMEText(contents, 'text')
+    html = MIMEText(contents, 'html')
+    #msg.attach(text)
+    msg.attach(html)
+
+    s = smtplib.SMTP(SENDER_SMTP)
+    s.ehlo()
+    s.starttls()
+    s.login(SENDER_EMAIL, SENDER_PASS)
+    s.sendmail(SENDER_EMAIL, TARGET_EMAIL, msg.as_string())
+    s.quit()
